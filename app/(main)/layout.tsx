30d663a78c107dc96d483aa512bd0b0056cdd2ba
pillaianandu155-ui <pillaianandu155@gmail.com>
2026-04-18 21:53:44 +0530

    logo final

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -239,7 +239,7 @@ export default function MainLayout({
               <p className="text-sm text-gray-500 max-w-xs leading-relaxed">
                 Disclaimer:
 
-We are an authorized marketing partner Maharashtra RERA ID: A51800046254 for this project. 
+We are an authorized marketing partner Maharashtra RERA ID: A011332503512 for this project. 
 Provided content is given by respective owners and this website and content is for information purpose only and it does not constitute any offer to avail for any services. 
 Prices mentioned are subject to change without prior notice and properties mentioned are subject to availability. 
 You can expect a call, SMS or emails on details registered with us.
